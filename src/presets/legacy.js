cbed0b30b80cb6d41302e4fcae89c1d8ffabea48
Hoyeung <hoyeungw@outlook.com>
2021-11-22 20:45:09 +0800

    removed presets

--- src/presets/legacy.js
+++
@@ -1,6 +0,0 @@
-// charge-bar legacy style as of 1.x
-export const preset = {
-  format: 'progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
-  barCompleteChar: '=',
-  barIncompleteChar: '-'
-}
--- src/presets/shades-classic.js
+++
@@ -1,6 +0,0 @@
-// charge-bar legacy style as of 1.x
-export const preset =  {
-    format: ' {bar} {percentage}% | ETA: {eta}s | {value}/{total}',
-    barCompleteChar: '\u2588',
-    barIncompleteChar: '\u2591'
-}
