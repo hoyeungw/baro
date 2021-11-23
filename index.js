afc0910c758d660b0a76af7981cbf42edd1f44ec
Hoyeung <hoyeungw@outlook.com>
2021-11-23 12:16:32 +0800

    export Spin to index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export { State }  from './src/State'
 export { Baro }   from './src/Baro'
 export { Escape } from './util/Escape'
 export { ETA }    from './util/ETA'
-
+export { Spin }   from './util/Spin'
 // export const Format = {
 //   Formatter: defaultFormatter,
 //   BarFormat: formatBar,
