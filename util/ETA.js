7d3c698be9c7aa3289ae7422c8682a5d350ce129
Hoyeung <hoyeungw@outlook.com>
2021-11-24 09:01:06 +0800

    simplified eta codes

--- util/ETA.js
+++ util/ETA.js
@@ -1,13 +1,11 @@
-// ETA calculation
+// ETA: estimated time to completion
 export class ETA {
-
   constructor(capacity, initTime, initValue) {
     // size of eta buffer
     this.capacity = capacity || 100
     // eta buffer with initial values
     this.valueSeries = [ initValue ]
     this.timeSeries = [ initTime ]
-
     // eta time value
     this.eta = '0'
   }
@@ -26,27 +24,22 @@ export class ETA {
   // eta calculation - request number of remaining events
   calculate(remaining) {
     // get number of samples in eta buffer
-    const consumed = this.valueSeries.length
-    const gap = Math.min(this.capacity, consumed)
-
-    const dValue = this.valueSeries[consumed - 1] - this.valueSeries[consumed - gap]
-    const dTime = this.timeSeries[consumed - 1] - this.timeSeries[consumed - gap]
-
-    // get progress per ms
-    const marginalRate = dValue / dTime
-
-    // strip past elements
-    this.valueSeries = this.valueSeries.slice(-this.capacity)
-    this.timeSeries = this.timeSeries.slice(-this.capacity)
-
-    // eq: vt_rate *x = total
-    const eta = Math.ceil(remaining / marginalRate / 1000)
-
-    // check values
+    const len = this.valueSeries.length,
+          cap = this.capacity,
+          lo  = len - 1,
+          hi  = len > cap ? len - cap : 0 // consumed-Math.min(cap,len)
+    const dValue = this.valueSeries[lo] - this.valueSeries[hi],
+          dTime  = this.timeSeries[lo] - this.timeSeries[hi],
+          rate   = dValue / dTime // get progress per ms
+    if (len > cap) {
+      this.valueSeries = this.valueSeries.slice(-cap)
+      this.timeSeries = this.timeSeries.slice(-cap)
+    } // strip past elements
+    const eta = Math.ceil(remaining / rate / 1000)
     return this.eta = isNaN(eta) ? 'NULL'
-      : !isFinite(eta) ? 'INF'  // +/- Infinity --- NaN already handled
-        : eta > 1e7 ? 'INF'  // > 10M s ? - set upper display limit ~115days (1e7/60/60/24)
-          : eta < 0 ? 0  // negative ?
+      : !isFinite(eta) ? 'INF'  // +/- Infinity: NaN already handled
+        : eta > 1e7 ? 'INF'  // > 10M s: - set upper display limit ~115days (1e7/60/60/24)
+          : eta < 0 ? 0  // negative: 0
             : eta  // assign
   }
 }
