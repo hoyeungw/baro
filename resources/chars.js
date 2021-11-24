51de555a194ddb88fe319e8e4427d0a2a5889a36
Hoyeung <hoyeungw@outlook.com>
2021-11-24 08:59:08 +0800

    renamed files

--- resources/chars.js
+++
@@ -1,3 +0,0 @@
-export const SHADE_CHARSET = [ `█`, `░` ] // \u2588 \u2591
-export const RECT_CHARSET = [ `■`, ` ` ] // \u25A0
-export const LEGACY_CHARSET = [ `=`, `-` ]
---
+++ resources/charset.js
@@ -0,0 +1,3 @@
+export const CHARSET_SHADE = [ `█`, `░` ] // \u2588 \u2591
+export const CHARSET_RECT = [ `■`, ` ` ] // \u25A0
+export const CHARSET_LEGACY = [ `=`, `-` ]
