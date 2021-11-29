e39dc96a02eb68a203f3b6de732609fcfdeb55f3
Hoyeung <hoyeungw@outlook.com>
2021-11-29 10:34:27 +0800

    conformed api

--- util/ETA.js
+++ util/ETA.js
@@ -7,36 +7,31 @@ export class ETA {
     this.valueSeries = [ initValue ]
     this.timeSeries = [ initTime ]
     // eta time value
-    this.eta = '0'
+    this.estimate = '0'
   }
 
   // add new values to calculation buffer
-  update(time, { value, total }) {
+  update({ now, value, total }) {
     this.valueSeries.push(value)
-    this.timeSeries.push(time)
-    // trigger recalculation
+    this.timeSeries.push(now ?? Date.now())
     this.calculate(total - value)
   }
 
-  // fetch estimated time
-  get value() { return this.eta }
-
   // eta calculation - request number of remaining events
   calculate(remaining) {
-    // get number of samples in eta buffer
-    const len = this.valueSeries.length,
+    const len = this.valueSeries.length, // get number of samples in eta buffer
           cap = this.capacity,
-          lo  = len - 1,
-          hi  = len > cap ? len - cap : 0 // consumed-Math.min(cap,len)
-    const dValue = this.valueSeries[lo] - this.valueSeries[hi],
-          dTime  = this.timeSeries[lo] - this.timeSeries[hi],
+          hi  = len - 1,
+          lo  = len > cap ? len - cap : 0 // consumed-Math.min(cap,len)
+    const dValue = this.valueSeries[hi] - this.valueSeries[lo],
+          dTime  = this.timeSeries[hi] - this.timeSeries[lo],
           rate   = dValue / dTime // get progress per ms
     if (len > cap) {
       this.valueSeries = this.valueSeries.slice(-cap)
       this.timeSeries = this.timeSeries.slice(-cap)
     } // strip past elements
-    const eta = Math.ceil(remaining / rate / 1000)
-    return this.eta = isNaN(eta) ? 'NULL'
+    const eta = Math.ceil(remaining / ( rate * 1000 ))
+    return this.estimate = isNaN(eta) ? 'NULL'
       : !isFinite(eta) ? 'INF'  // +/- Infinity: NaN already handled
         : eta > 1e7 ? 'INF'  // > 10M s: - set upper display limit ~115days (1e7/60/60/24)
           : eta < 0 ? 0  // negative: 0
