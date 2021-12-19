fd0f76cbc075927e52affe16f020e427d0629a14
Hoyeung <hoyeungw@outlook.com>
2021-12-19 20:58:15 +0800

    introduced locker to prevent multiple starts that may cause memory problem

--- src/Baro.js
+++ src/Baro.js
@@ -1,22 +1,22 @@
-import { Escape }   from '../util/Escape'
-import { Terminal } from '../util/Terminal'
-import { Config }   from './Config'
-import { Layout }   from './Layout'
-import { State }    from './State'
+import { cursor } from '@arpel/escape'
+import { Escape } from '../util/Escape.js'
+import { IO }     from '../util/IO.js'
+import { Config } from './Config.js'
+import { Layout } from './Layout.js'
+import { State }  from './State.js'
 
 // Baro constructor
 export class Baro {
-  /** @type {Config|object} store config */
   config
-  /** @type {Terminal|object} store terminal instance */
-  terminal
-  /** @type {boolean} progress bar active ? */
-  active = false
-  /** @type {function} use default format or custom one ? */
   format
-  /** @type {[State]}  */
-  states
-
+  states = []
+  escape = Escape.build({
+    fn: this.#renderStates,
+    ctx: this,
+    arg: this.states
+  })
+  #locker = null
+  offset = 0
   /**
    *
    * @param {Config} config
@@ -25,13 +25,7 @@ export class Baro {
   constructor(config, layout) {
     this.config = config
     this.layout = layout
-    this.states = []
-    this.escape = Escape.build({
-      fn: this.#renderStates,
-      ctx: this,
-      arg: this.states
-    })
-    this.terminal = this.config.terminal ?? new Terminal(this.config)
+    this.io = IO.build(this.config)
   }
 
   static build(config, layout) {
@@ -40,17 +34,30 @@ export class Baro {
     return new Baro(config, layout)
   }
 
-  get active() { return !!this.escape.timer }
+  get active() { return this.escape.active }
 
   get forceRedraw() {
-    return this.config.forceRedraw || ( this.config.noTTYOutput && !this.terminal.isTTY ) // force redraw in noTTY-mode!
+    return this.config.forceRedraw || ( this.config.noTTYOutput && !this.io.isTTY ) // force redraw in noTTY-mode!
   }
 
-  get noTTY() { return this.config.noTTYOutput && !this.terminal.isTTY }
+  async start() {
+    const { io } = this
+    io.input.resume()
+    if (this.config.hideCursor) io.showCursor(false) // hide the cursor ?
+    if (!this.config.lineWrap) io.setLineWrap(false) // disable line wrapping ?
+    // this.io.output.write('\f')
+    const height = this.io.height
+    this.io.output.write(cursor.nextLine(height) + cursor.prevLine(height))
+    // this.io.output.write(scroll.down(height))
 
-  boot() {
-    if (this.config.hideCursor) this.terminal.showCursor(false) // hide the cursor ?
-    if (!this.config.lineWrap) this.terminal.setLineWrap(false) // disable line wrapping ?
+    // const [ x, y ] = await io.asyncCursorPos()
+    // console.log('x', x, 'y', y, 'offset', this.offset, 'states', this.states.length, 'height', io.height)
+    // if (x + this.states.length >= io.height) {
+    //   io.output.write('\f')
+    //   // io.nextPage()
+    // }
+
+    // WARNING: intentionally call loop without await
     this.escape.loop(this.config.throttle) // initialize update timer
   }
 
@@ -59,58 +66,72 @@ export class Baro {
    * @param {State|object} state // const state = new State(total, value, this.config.eta)
    * @returns {State|object}
    */
-  append(state) {
-    // progress updates are only visible in TTY mode!
-    if (this.noTTY) return void 0
-    state.last = Number.NEGATIVE_INFINITY
-    this.states.push(state)
-    if (!this.escape.active && this.states.length) this.boot()
+  async append(state) {
+    if (this.#locker) await this.#locker // console.debug('>>', state.agent, 'waiting for occupy')
+    const taskPromise = Promise
+      .resolve()
+      .then(async () => {
+        state.last = Number.NEGATIVE_INFINITY
+        this.states.push(state)
+        if (!this.escape.active && this.states.length) await this.start()
+      })
+    this.#locker = taskPromise.then(() => this.#locker = null)
     return state
   }
 
   // remove a bar from the stack
   remove(state) {
     const index = this.states.indexOf(state) // find element
-    if (index < 0) { return false } // element found ?
+    if (index < 0) return false // element found ?
     this.states.splice(index, 1) // remove element
-    this.terminal.newline()
-    this.terminal.clearDown()
+    this.io.nextLine()
+    this.io.clearDown()
     return true
   }
 
-  stop() {
+  async stop() {
     this.escape.stop() // stop timer
-    if (this.config.hideCursor) { this.terminal.showCursor(true) } // cursor hidden ?
-    if (!this.config.lineWrap) { this.terminal.setLineWrap(true) } // re-enable line wrapping ?
+    if (this.config.hideCursor) { this.io.showCursor(true) } // cursor hidden ?
+    if (!this.config.lineWrap) { this.io.setLineWrap(true) } // re-enable line wrapping ?
     if (this.config.autoClear) {
-      this.terminal.resetCursor() // reset cursor
-      this.terminal.clearDown()
+      this.io.resetCursor() // reset cursor
+      this.io.clearDown()
     } // clear all bars or show final progress
     else {
       // for (let state of this.states) { state.stop() }
-      this.#renderStates(this.states)
+      await this.#renderStates(this.states)
     }
+
+    this.io.input.pause()
   }
 
-  #renderStates(states) {
-    this.terminal.resetCursor() // reset cursor
-    // if (this.terminal.excessive) { this.terminal.write(`${CSI}2;${this.terminal.height - 1}${DECSTBM}`) }
-    const max = this.terminal.height
-    for (let hi = states.length, i = hi > max ? hi - max + 1 : 0; i < hi; i++) {
-      const state = states[i] // update each bar
-      if (this.forceRedraw || ( state.value !== state.last )) {
-        this.terminal.cleanWrite(this.layout.format(state))
-        state.last = state.value
-      } // string updated, only trigger redraw on change
-      this.terminal.newline()
+  async #renderStates(states) {
+    const { io } = this
+    const height = io.height - 1
+    const [ x, y ] = await io.asyncCursorPos()
+    // if (!this.busy && ( x + states.length > height )) {
+    //   io.nextPage()
+    //   this.offset = 0
+    // }
+    // else {
+    //   this.busy = true
+    //
+    // }
+
+    io.offsetLines(-Math.min(this.offset, height)) // reset cursor
+    this.offset = 0
+    if (height) {
+      for (const state of states.slice(-( height ))) {
+        if (this.forceRedraw || ( state.value !== state.last )) {
+          io.writeOff(`CURSOR (${x}, ${y}) OFFSET (${this.offset}) TERM (${io.size}) ` + this.layout.format(state))
+          state.last = state.value
+        }
+        io.nextLine()
+        this.offset++
+      }
     }
-    // if (this.terminal.excessive) { this.terminal.write(`${CSI}${DECSTBM}`) }
-    if (this.noTTY) {
-      this.terminal.newline()
-      this.terminal.newline()
-    } // add new line in noTTY mode
     if (this.config.autoStop && states.every(state => state.reachLimit)) {
-      this.stop()
+      await this.stop()
     } // stop if autoStop and all bars stopped
   }
 }
